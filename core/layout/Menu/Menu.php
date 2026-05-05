76a6705b6f6fe0c40e6eed7c6e0fd321cdc93cc1
matin <matin.afzali.dev@gmail.com>
2026-05-05 10:12:48 -0800

    Implement Menu

--- core/layout/Menu/Menu.php
+++ core/layout/Menu/Menu.php
@@ -1,1 +1,17 @@
-<?php
+<aside class="col-md-3 col-lg-2 bg-light border-start navbar-expand-lg p-3 d-flex flex-column align-items-center position-fixed end-0 top-0 min-vh-100">
+    <div class="text-center mb-3">
+        <div class="rounded-circle bg-secondary" style="width:80px; height:80px;"></div>
+        <div class="border mt-2 p-1">نام کاربر</div>
+    </div>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <nav class="w-100">
+        <a href="#" class="btn btn-outline-dark w-100 mb-2">منو ۱</a>
+        <a href="#" class="btn btn-outline-dark w-100 mb-2">منو ۲</a>
+        <a href="#" class="btn btn-outline-dark w-100 mb-2">منو ۳</a>
+        <a href="#" class="btn btn-outline-dark w-100 mb-2">منو ۴</a>
+        <a href="#" class="btn btn-outline-dark w-100 mb-2">منو ۵</a>
+    </nav>
+</aside>
