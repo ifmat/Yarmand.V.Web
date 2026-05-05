98308e5a5f06a522ac708e7e9dc5afd3aed897af
matin <matin.afzali.dev@gmail.com>
2026-05-05 10:12:21 -0800

    Improve index for testing

--- index.php
+++ index.php
@@ -1,1 +1,19 @@
 <?php
+require_once 'core/layout/Html/StartHtml.php';
+require_once 'core/layout/Head/Head.php';
+?>
+    <body>
+    <?php
+    require_once 'core/layout/Header/Header.php';
+    require_once 'core/layout/Menu/Menu.php';
+    ?>
+    <main class="col-md-9 col-lg-10 p-4">
+    <div class="bg-danger mt-5">Lorem.</div>
+    </main>
+    <?php
+    require_once 'core/layout/Js/Js.php';
+    ?>
+    </body>
+<?php
+require_once 'core/layout/Html/EndHtml.php';
+?>
