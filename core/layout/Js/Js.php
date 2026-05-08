36ae1f5892ffa39d2e229e4c1c3dfc9c513d33f6
matin <matin.afzali.dev@gmail.com>
2026-05-08 09:36:02 -0800

    Implement Add Files Icon

--- core/layout/Js/Js.php
+++ core/layout/Js/Js.php
@@ -1,2 +1,3 @@
 <script src="assets/js/vendors.min.js"></script>
 <script src="assets/vendor_components/apexcharts-bundle/irregular-data-series.js"></script>
+<script src="assets/icons/feather-icons/feather.min.js"></script>
