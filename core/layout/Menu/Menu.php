16e0bbc62a88ad2e4da3a8f906cab033b3183edb
matin <matin.afzali.dev@gmail.com>
2026-05-08 09:26:48 -0800

    Improve Ul and Li

--- core/layout/Menu/Menu.php
+++ core/layout/Menu/Menu.php
@@ -1,19 +1,28 @@
 <aside class="col-md-3 col-lg-2 bg-light border-start navbar-expand-lg p-3 d-flex flex-column align-items-center position-fixed end-0 top-0 min-vh-100">
     <div class="text-center mb-3">
-        <div class="w-100" >
-            <img src="assets/images/avatar.png" class="w-50 bg-success  rounded rounded-circle">
+        <div class="w-100">
+            <img src="assets/images/avatar.png" class="w-50 bg-success rounded rounded-circle" alt="">
         </div>
-        <div class=" mt-2 p-1">نام کاربر</div>
+        <div class="mt-3 font-monospace mb-2 p-1">Matin</div>
     </div>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
             aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
     <nav class="w-100">
-        <a href="#" class="btn btn-outline-dark w-100 mb-2">منو ۱</a>
-        <a href="#" class="btn btn-outline-dark w-100 mb-2">منو ۲</a>
-        <a href="#" class="btn btn-outline-dark w-100 mb-2">منو ۳</a>
-        <a href="#" class="btn btn-outline-dark w-100 mb-2">منو ۴</a>
-        <a href="#" class="btn btn-outline-dark w-100 mb-2">منو ۵</a>
+        <ul class="without-bullets">
+            <li><a href="#" class="btn btn-outline-success border-dark text-dark w-100 mb-2 ">منو ۱</a>
+            </li>
+            <li><a href="#" class="btn btn-outline-success border-dark text-dark w-100 mb-2 ">منو ۱</a>
+            </li>
+            <li><a href="#" class="btn btn-outline-success border-dark text-dark w-100 mb-2 ">منو ۱</a>
+            </li>
+            <li><a href="#" class="btn btn-outline-success border-dark text-dark w-100 mb-2 ">منو ۱</a>
+            </li>
+            <li><a href="#" class="btn btn-outline-success border-dark text-dark w-100 mb-2 ">منو ۱</a>
+            </li>
+
+        </ul>
+
     </nav>
 </aside>
