7ed6caecfe30a76ed813f2ea0d8b7e3b59e915fb
matin <matin.afzali.dev@gmail.com>
2026-05-08 09:45:10 -0800

    Implement Icons And Add files Icons And fixed Src and Herf

--- core/layout/Head/Head.php
+++ core/layout/Head/Head.php
@@ -3,5 +3,6 @@
     <link rel="stylesheet" href="assets/css/vendors_css.css">
     <link rel="stylesheet" href="assets/css/style.css">
     <link rel="stylesheet" href="assets/css/skin_color.css">
+    <link rel="stylesheet" href="assets/icons/font-awesome/css/font-awesome.css">
     <title>Document</title>
 </head>
