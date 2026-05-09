a7dec940c172215644b0d5390a18ba0c7dcc0a46
matin <matin.afzali.dev@gmail.com>
2026-05-09 08:37:07 -0800

    Improve Translate Add words

--- assets/js/language/translations.js
+++ assets/js/language/translations.js
@@ -2,12 +2,16 @@ export const translations = {
     fa: {
         title: "سلام",
         Welcome: "خوش امدید",
+        item_menu: "ادرس یک صفحه",
+        item_Delete_menu: "خروج",
         welcome: "به سایت ما خوش آمدید",
         button: "ورود"
     },
     en: {
         title: "Hello",
         Welcome: "Welcome",
+        item_menu: "Address Pages",
+        item_Delete_menu: "logout",
         welcome: "Welcome to our website",
         button: "Login"
     }
