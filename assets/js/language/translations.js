62a5e7cac1b1531cb06ba90adf13ddf3e42fdc2f
matin <matin.afzali.dev@gmail.com>
2026-05-09 08:27:59 -0800

    Improve Translate file Add New word

--- assets/js/language/translations.js
+++ assets/js/language/translations.js
@@ -1,11 +1,13 @@
 export const translations = {
     fa: {
         title: "سلام",
+        Welcome: "خوش امدید",
         welcome: "به سایت ما خوش آمدید",
         button: "ورود"
     },
     en: {
         title: "Hello",
+        Welcome: "Welcome",
         welcome: "Welcome to our website",
         button: "Login"
     }
