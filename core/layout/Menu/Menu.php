d7ff32115beca104739263381ae08473ce181e55
matin <matin.afzali.dev@gmail.com>
2026-05-19 05:13:56 -0800

    Improve Menu Redesign

--- core/layout/Menu/Menu.php
+++ core/layout/Menu/Menu.php
@@ -1,29 +1,59 @@
-<aside class="col-md-3 col-lg-2 bg-light border-start navbar-expand-lg p-3 d-flex flex-column align-items-center position-fixed end-0 top-0 min-vh-100">
-    <div class="text-center mb-3">
-        <button id="btn-fa">فارسی</button>
-        <button id="btn-en">English</button>
-        <div class="w-100">
-            <img src="assets/images/avatar.png" class="w-50 bg-success rounded rounded-circle" alt="">
-        </div>
-        <div class="mt-3 font-monospace mb-2 p-1 d-flex"><p Multi_Lang="Welcome"></p> : Matin </div>
-    </div>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-    <nav class="w-100 menu">
-        <ul class="without-bullets justify-content-center align-items-center mx-auto">
-            <li><a href="#" class="btn btn-outline-success border-dark text-dark w-100 mb-2 "><span class="fa-solid fa-house" Multi_Lang="traneh" ></span></a>
-            </li>
-            <li><a href="#" class="btn btn-outline-success border-dark text-dark w-100 mb-2 "><span class="fa-solid fa-user" Multi_Lang="item_menu"></span></a>
-            </li>
-            <li><a href="#" class="btn btn-outline-success border-dark text-dark w-100 mb-2 "><span class="fa-solid fa-chart-line" Multi_Lang="item_menu"> </span></a>
-            </li>
-            <li><a href="#" class="btn btn-outline-success border-dark text-dark w-100 mb-2 "><span class="fa-solid fa-gear" Multi_Lang="item_menu"> </span></a>
-            </li>
-            <li><a href="#" class="btn btn-outline-success border-danger text-danger w-100 mb-2 "><span class="fa-solid fa-right-from-bracket" Multi_Lang="item_Delete_menu"></span></a>
-            </li>
-        </ul>
+<aside class="sidebar col-md-3 col-lg-2 position-fixed end-0 top-0 min-vh-100 d-flex flex-column">
 
+    <div class="sidebar-header text-center">
+        <div class="lang-switch mb-3">
+            <button id="btn-fa" class="btn btn-sm btn-outline-light">فارسی</button>
+            <button id="btn-en" class="btn btn-sm btn-outline-light">English</button>
+        </div>
+
+        <div class="avatar-wrapper">
+            <img src="assets/images/avatar.png" alt="Avatar">
+        </div>
+
+        <div class="welcome-text mt-3">
+            <p Multi_Lang="Welcome" class="mb-1"></p>
+            <span class="fw-bold">Matin</span>
+        </div>
+    </div>
+
+    <nav class="menu mt-4 w-100">
+        <ul class="list-unstyled w-100 px-3">
+
+            <li>
+                <a href="#" class="menu-item active">
+                    <i class="fa-solid fa-house"></i>
+                    <span Multi_Lang="traneh"></span>
+                </a>
+            </li>
+
+            <li>
+                <a href="#" class="menu-item">
+                    <i class="fa-solid fa-user"></i>
+                    <span Multi_Lang="item_menu"></span>
+                </a>
+            </li>
+
+            <li>
+                <a href="#" class="menu-item">
+                    <i class="fa-solid fa-chart-line"></i>
+                    <span Multi_Lang="item_menu"></span>
+                </a>
+            </li>
+
+            <li>
+                <a href="#" class="menu-item">
+                    <i class="fa-solid fa-gear"></i>
+                    <span Multi_Lang="item_menu"></span>
+                </a>
+            </li>
+
+            <li>
+                <a href="#" class="menu-item logout">
+                    <i class="fa-solid fa-right-from-bracket"></i>
+                    <span Multi_Lang="item_Delete_menu"></span>
+                </a>
+            </li>
+
+        </ul>
     </nav>
 </aside>
