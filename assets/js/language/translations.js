e5beb195bb44cc8d469d241f8360951d1e55c493
matin <matin.afzali.dev@gmail.com>
2026-05-22 07:54:30 -0800

    Improve Translate Add New Word

--- assets/js/language/translations.js
+++ assets/js/language/translations.js
@@ -1,18 +1,37 @@
 export const translations = {
     fa: {
         title: "سلام",
-        Welcome: "خوش امدید",
-        item_menu: "ادرس یک صفحه",
+        Welcome: "خوش آمدید",
+        item_menu: "آدرس صفحات",
         item_Delete_menu: "خروج",
         welcome: "به سایت ما خوش آمدید",
-        button: "ورود"
+        Role_do: "ورود",
+        Role_salmand: "سالمند",
+        Role_choose: "نقش خود را انتخاب کنید",
+        Welcome_login: "ورود به سامانه",
+        Role_shop: "فروشگاه",
+        Label_Name: "نام",
+        Label_LastName: "نام خانوادگی",
+        Label_Role: "نقش کاربری",
+        Label_Email: "ایمیل",
+        Label_Password: "رمز عبور"
     },
     en: {
         title: "Hello",
         Welcome: "Welcome",
         item_menu: "Address Pages",
-        item_Delete_menu: "logout",
+        item_Delete_menu: "Logout",
         welcome: "Welcome to our website",
-        button: "Login"
+        button: "Login",
+        Role_do: "Login",
+        Role_choose: "Select your role",
+        Welcome_login: "Welcome To Platform",
+        Role_salmand: "Senior",
+        Role_shop: "Shop",
+        Label_Name: "First Name",
+        Label_LastName: "Last Name",
+        Label_Role: "Role",
+        Label_Email: "Email",
+        Label_Password: "Password"
     }
 };
