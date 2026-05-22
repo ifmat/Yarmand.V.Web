c33f499a2b471210c547cab7f35a96d8bed6f362
matin <matin.afzali.dev@gmail.com>
2026-05-22 05:42:49 -0800

    Implement Header

--- core/layout/Header/Header.php
+++ core/layout/Header/Header.php
@@ -1,1 +1,39 @@
-<?php
+<header class="main-header d-flex align-items-center justify-content-between px-4">
+
+    <div class="d-flex align-items-center gap-3">
+
+        <button id="sidebarToggleBtn">
+            <i class="fa fa-bars"></i>
+        </button>
+
+        <div class="logo fw-bold">
+            یارمند
+        </div>
+
+    </div>
+
+
+    <nav class="header-menu d-none d-lg-block">
+        <ul class="d-flex gap-4 m-0 p-0 list-unstyled">
+            <li><a href="#">خانه</a></li>
+            <li><a href="#">محصولات</a></li>
+            <li><a href="#">خدمات</a></li>
+            <li><a href="#">درباره ما</a></li>
+            <li><a href="#">تماس</a></li>
+        </ul>
+    </nav>
+
+
+    <div class="header-actions d-flex align-items-center gap-3">
+
+        <div class="search-box">
+            <input type="text" placeholder="جستجو...">
+        </div>
+
+        <i class="fa fa-user action-icon"></i>
+        <i class="fa fa-heart action-icon"></i>
+        <i class="fa fa-shopping-cart action-icon"></i>
+
+    </div>
+
+</header>
