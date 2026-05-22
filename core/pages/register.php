0379aa3f4d62a64effe2129c0ba975e6fc9a009a
matin <matin.afzali.dev@gmail.com>
2026-05-22 07:53:16 -0800

    Improve Register Edite Text To Multi Lang

--- core/pages/register.php
+++ core/pages/register.php
@@ -7,53 +7,77 @@ require_once '../layout/Head/Head.php';
     <div class="container">
         <div class="row justify-content-center ">
             <div class="col-11 col-sm-8 col-md-7 col-lg-7 col-xl-6">
-
                 <!-- کارت ثبت نام / ورود -->
                 <div class="card border-0 shadow-lg rounded-4 overflow-hidden Register-main">
-
                     <!-- هدر کارت -->
                     <div class="card-header text-center py-4 border-0"
-                         style="background: linear-gradient(135deg, #00ec9d, #00b894);">
+                         style="">
                         <i class="ti-lock display-5 text-white"></i>
-                        <h2 class="text-white mt-2 mb-0 fw-bold">ورود به سامانه</h2>
+                        <h2 class="text-white mt-2 mb-0 fw-bold" Multi_Lang="Welcome_login"></h2>
                     </div>
-
                     <!-- بدنه کارت -->
                     <div class="card-body p-4 p-xl-5">
                         <form action="chek_user" method="post">
-
-                            <!-- فیلد نام کاربری -->
-                            <div class="input-group mb-4">
-                                <input name="user" id="user" type="number"
-                                       class="form-control form-control-lg rounded-3  text-dark "
-                                       placeholder="نام کاربری">
-                                <span class="input-group-text rounded-3 border-0">
-                                <i class="fa-solid fa-user text-dark"></i>
-                            </span>
+                            <!-- نام -->
+                            <div class="mb-3">
+                                <label for="FirstName" class="form-label fw-bold" Multi_Lang="Label_Name">نام</label>
+                                <div class="input-group">
+                                    <input name="FirstName" id="FirstName" type="text" class="form-control form-control-lg rounded-3 text-dark">
+                                    <span class="input-group-text rounded-3 border-0"><i class="fa-solid fa-user text-dark"></i></span>
+                                </div>
                             </div>
 
-                            <!-- فیلد رمز عبور -->
-                            <div class="input-group mb-4 ">
-                                <input id="pass" name="pass" type="password"
-                                       class="form-control form-control-lg rounded-3 "
-                                       placeholder="رمز عبور">
-                                <span class="input-group-text toggle-password rounded-3 border-0">
-                                <i class="fa-solid fa-eye text-dark"></i>
-                            </span>
+                            <!-- نام خانوادگی -->
+                            <div class="mb-3">
+                                <label for="LastName" class="form-label fw-bold Register-label" Multi_Lang="Label_LastName"></label>
+                                <div class="input-group">
+                                    <input name="LastName" id="LastName" type="text" class="form-control form-control-lg rounded-3 text-dark">
+                                    <span class="input-group-text rounded-3 border-0"><i class="fa-solid fa-user text-dark"></i></span>
+                                </div>
                             </div>
 
-                            <!-- دکمه ورود -->
+                            <!-- نقش کاربری -->
+                            <div class="mb-3">
+                                <label for="role" class="form-label fw-bold" Multi_Lang="Label_Role" ></label>
+                                <div class="input-group">
+                                    <select name="role" id="role" class="form-control form-control-lg rounded-3 text-dark">
+                                        <option value="" disabled selected Multi_Lang="Role_Choise"></option>
+                                        <option value="1" Multi_Lang="Role_salmand"></option>
+                                        <option value="2" Multi_Lang="Role_do"></option>
+                                        <option value="3" Multi_Lang="Role_shop"></option>
+                                    </select>
+                                    <span class="input-group-text rounded-3 border-0"><i class="fa-solid fa-universal-access text-dark"></i></span>
+                                </div>
+                            </div>
+
+                            <!-- ایمیل -->
+                            <div class="mb-3">
+                                <label for="Email" class="form-label fw-bold" Multi_Lang="Label_Email">ایمیل</label>
+                                <div class="input-group">
+                                    <input name="Email" id="Email" type="email" class="form-control form-control-lg rounded-3 text-dark">
+                                    <span class="input-group-text rounded-3 border-0"><i class="fa-solid fa-envelope text-dark"></i></span>
+                                </div>
+                            </div>
+
+                            <!-- رمز عبور -->
+                            <div class="mb-3">
+                                <label for="pass" class="form-label fw-bold" Multi_Lang="Label_Password">رمز عبور</label>
+                                <div class="input-group">
+                                    <input id="pass" name="pass" type="password" class="form-control form-control-lg rounded-3">
+                                    <span class="input-group-text toggle-password rounded-3 border-0">
+            <i class="fa-solid fa-eye text-dark"></i>
+        </span>
+                                </div>
+                            </div>
+
                             <button type="submit" name="login"
                                     class="btn w-100 py-2 fw-bold text-white border-0 rounded-3"
                                     style="">
                                 ورود
                             </button>
-
                         </form>
                     </div>
-
                 </div>
-
             </div>
         </div>
     </div>
