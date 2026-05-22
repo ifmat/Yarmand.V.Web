b521afd549c30572d89d23c2c5c50a76ed89c0fe
matin <matin.afzali.dev@gmail.com>
2026-05-22 06:43:24 -0800

    Improve Connect Connecting TO Database

--- core/db/Connect.php
+++ core/db/Connect.php
@@ -1,1 +1,8 @@
 <?php
+$conn = mysqli_connect('localhost', 'root', '', 'yarmand');
+mysqli_set_charset($conn, "utf8mb4");
+
+if ($conn->connect_error) {
+    error_log("DB connection failed: " . $conn->connect_error);
+    die("متاسفیم، اتصال به پایگاه داده امکان‌پذیر نیست.");
+}
