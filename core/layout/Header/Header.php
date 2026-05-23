22970747177a37cf666cf69daff7f2b55d46a049
matin <matin.afzali.dev@gmail.com>
2026-05-23 11:04:03 -0800

    Improve Header Add Button Multi_lang

--- core/layout/Header/Header.php
+++ core/layout/Header/Header.php
@@ -7,13 +7,26 @@
     </div>
 
     <div class="header-left">
+        <!-- باکس جستجو -->
         <div class="search-box">
             <input type="text" placeholder="جستجو...">
             <i class="fas fa-search"></i>
         </div>
-        <div class="user-profile">
-                                <img src="assets/images/avatar.png" alt="User" class="avatar">
-            <span>ماتین</span>
+
+        <!-- بخش پروفایل و زبان -->
+        <div class="header-actions">
+            <!-- دکمه‌های تغییر زبان -->
+            <div class="lang-switch">
+                <button id="btn-fa" class="lang-btn active">فا</button>
+                <button id="btn-en" class="lang-btn">En</button>
+            </div>
+
+            <!-- پروفایل کاربر -->
+            <div class="user-profile">
+                <img src="assets/images/avatar.png" alt="User" class="avatar">
+                <span class="user-name-header">ماتین</span>
+                <i class="fas fa-chevron-down arrow-icon"></i>
+            </div>
         </div>
     </div>
 </header>
