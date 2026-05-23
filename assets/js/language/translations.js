d8d5ced914c190c18347db3f74ab1dc5ca67e55b
matin <matin.afzali.dev@gmail.com>
2026-05-23 09:41:00 -0800

    Improve Translate Add New Words For Menu

--- assets/js/language/translations.js
+++ assets/js/language/translations.js
@@ -20,6 +20,67 @@ export const translations = {
         register_Prompt: "آیا حساب ندارید؟",
         register_Link: "ساخت حساب",
         Submit: "ورود",
+        admin_role: "مدیر سیستم",
+        doer_role: "انجام دهنده",
+        elderly_role: "سالمند",
+        shop_role: "فروشگاه",
+        support_role: "پشتیبان تلفنی",
+        guest_role: "کاربر مهمان",
+
+        // منوها
+        main_menu: "منوی اصلی",
+        account_menu: "حساب کاربری",
+
+        // آیتم‌های منو - عمومی
+        menu_home: "خانه",
+        menu_profile: "پروفایل",
+        menu_logout: "خروج",
+        menu_wallet: "کیف پول",
+
+        // ادمین
+        menu_register_elderly: "ثبت سالمند",
+        menu_register_shop: "ثبت فروشگاه",
+        menu_list_elderly: "لیست سالمندها",
+        menu_verify_doer: "تایید انجام دهنده",
+        menu_list_doers: "لیست انجام دهنده‌ها",
+        menu_list_requests: "لیست تمام درخواست‌ها",
+        menu_rate_doers: "لیست امتیازبندی انجام دهنده‌ها",
+        menu_register_reward: "ثبت جایزه‌های سیستم",
+        menu_register_support_call: "ثبت پشتیبان تلفنی",
+        menu_list_shops: "لیست فروشگاه‌ها",
+        menu_list_products: "لیست تمامی محصولات ثبت شده",
+        menu_system_wallet: "نمایش موجودی حساب سیستم",
+        menu_system_coin: "سیستم کوین",
+
+        // انجام دهنده
+        menu_current_requests: "در حال انجام - لیست درخواست‌ها",
+        menu_list_all_requests: "لیست تمام درخواست‌ها",
+
+        // سالمند
+        menu_submit_request: "ثبت درخواست برای سالمند",
+        menu_track_request: "پیگیری درخواست",
+        menu_rate_doer: "ثبت امتیاز برای انجام دهنده",
+        menu_need_verify: "نیاز به تایید شدن",
+
+        // فروشگاه
+        menu_register_product: "ثبت محصول",
+        menu_list_all_products: "لیست تمام محصول ها",
+        menu_register_off_product: "ثبت تخفیف روی محصول‌ها",
+        menu_profile_shop: "پروفایل فروشگاه",
+
+        // پشتیبان تلفنی
+        menu_list_shop_products: "لیست محصول‌های فروشگاه",
+
+        // صفحات عمومی
+        menu_about_us: "درباره ما",
+        menu_contact_us: "ارتباط با ما",
+        menu_rules: "قوانین ما",
+        menu_faq: "سوال های متداول",
+        menu_register: "ثبت نام",
+        menu_login: "ورود",
+        menu_products_for_elderly: "نمایش صفحه محصول به سالمند",
+        menu_product_details: "صفحه جزئیات محصول",
+        menu_requests_for_doer: "نمایش صفحه درخواست‌ها برای انجام دهنده"
     },
     en: {
         title: "Hello",
@@ -43,5 +104,52 @@ export const translations = {
         register_Prompt: "Do you Not already have an account?",
         register_Link: "Sing up",
         Submit: "Submit",
+        main_menu: "Main Menu",
+        account_menu: "User Account",
+
+        menu_home: "Home",
+        menu_profile: "Profile",
+        menu_logout: "Logout",
+        menu_wallet: "Wallet",
+
+        menu_register_elderly: "Register Elderly",
+        menu_register_shop: "Register Shop",
+       menu_list_elderly: "List of Elderly",
+        menu_verify_doer: "Verify Doer",
+        menu_list_doers: "List of Doers",
+        menu_list_requests: "List of All Requests",
+        menu_rate_doers: "List of Doer Ratings",
+        menu_register_reward: "Register System Rewards",
+        menu_register_support_call: "Register Phone Support",
+        menu_list_shops: "List of Shops",
+        menu_list_products: "List of All Registered Products",
+        menu_system_wallet: "System Wallet Balance",
+        enu_system_coin: "System Coin",
+
+        menu_current_requests: "Currently Doing - Request List",
+        menu_list_all_requests: "List of All Requests",
+
+        menu_submit_request: "Submit Request for Elderly",
+        menu_track_request: "Track Request",
+        menu_rate_doer: "Rate Doer",
+        menu_need_verify: "Needs Verification",
+
+        menu_register_product: "Register Product",
+        menu_list_all_products: "List of All Products",
+        menu_register_off_product: "Register Product Discount",
+        menu_profile_shop: "Shop Profile",
+        menu_list_shop_products: "List of Shop Products",
+        menu_about_us: "About Us",
+        menu_contact_us: "Contact Us",
+        menu_rules: "Our Rules",
+        menu_faq: "FAQ",
+        menu_register: "Register",
+        menu_login: "Login",
+        menu_products_for_elderly: "Show Product Page for Elderly",
+        menu_product_details: "Product Details Page",
+        menu_requests_for_doer: "Show Request Page for Doer"
     }
-};
+
+
+}
+;
