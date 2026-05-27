7016e6e543a731017370c95478533b2e4a4d7229
matin <matin.afzali.dev@gmail.com>
2026-05-27 08:36:47 -0800

    Improve Menu Rename Link

--- core/layout/Menu/Menu.php
+++ core/layout/Menu/Menu.php
@@ -28,23 +28,23 @@ switch ($_SESSION['level']) {
     <ul class="sidebar-menu">
         <li class="menu-title" Multi_lang="main_menu"></li>
         <li><a href="dashboard" class="active"><i class="fas fa-home"></i> <span Multi_lang="menu_home"></span></a></li>
-        <li><a href="register_elderly"><i class="fas fa-user-plus"></i> <span Multi_lang="menu_register_elderly"></span></a></li>
-        <li><a href="register_shop"><i class="fas fa-store"></i> <span Multi_lang="menu_register_shop"></span></a></li>
-        <li><a href="list_elderly"><i class="fas fa-users"></i> <span Multi_lang="menu_list_elderly"></span></a></li>
+        <li><a href="Register_yarjo"><i class="fas fa-user-plus"></i> <span Multi_lang="menu_register_elderly"></span></a></li>
+        <li><a href="Register_Camp_Yarjo"><i class="fas fa-store"></i> <span Multi_lang="menu_register_shop"></span></a></li>
+        <li><a href="List_Users"><i class="fas fa-users"></i> <span Multi_lang="menu_list_elderly"></span></a></li>
         <li><a href="verify_doer"><i class="fas fa-check-circle"></i> <span Multi_lang="menu_verify_doer"></span></a></li>
-        <li><a href="list_doers"><i class="fas fa-list"></i> <span Multi_lang="menu_list_doers"></span></a></li>
-        <li><a href="list_requests"><i class="fas fa-clipboard-list"></i> <span Multi_lang="menu_list_requests"></span></a></li>
-        <li><a href="rate_doers"><i class="fas fa-star"></i> <span Multi_lang="menu_rate_doers"></span></a></li>
+        <li><a href="List_Users"><i class="fas fa-list"></i> <span Multi_lang="menu_list_doers"></span></a></li>
+        <li><a href="List_Request"><i class="fas fa-clipboard-list"></i> <span Multi_lang="menu_list_requests"></span></a></li>
+        <li><a href="ScoreBord"><i class="fas fa-star"></i> <span Multi_lang="menu_rate_doers"></span></a></li>
         <li><a href="register_reward"><i class="fas fa-gift"></i> <span Multi_lang="menu_register_reward"></span></a></li>
         <li><a href="register_support_call"><i class="fas fa-phone-alt"></i> <span Multi_lang="menu_register_support_call"></span></a></li>
-        <li><a href="list_shops"><i class="fas fa-store"></i> <span Multi_lang="menu_list_shops"></span></a></li>
-        <li><a href="list_products"><i class="fas fa-boxes"></i> <span Multi_lang="menu_list_products"></span></a></li>
-        <li><a href="system_wallet"><i class="fas fa-wallet"></i> <span Multi_lang="menu_system_wallet"></span></a></li>
+        <li><a href="List_Users"><i class="fas fa-store"></i> <span Multi_lang="menu_list_shops"></span></a></li>
+        <li><a href="List_Product_Shop"><i class="fas fa-boxes"></i> <span Multi_lang="menu_list_products"></span></a></li>
+        <li><a href="Wallet"><i class="fas fa-wallet"></i> <span Multi_lang="menu_system_wallet"></span></a></li>
         <li><a href="system_coin"><i class="fas fa-coins"></i> <span Multi_lang="menu_system_coin"></span></a></li>
         
         <li class="menu-title" Multi_lang="account_menu"></li>
-        <li><a href="profile"><i class="fas fa-user-circle"></i> <span Multi_lang="menu_profile"></span></a></li>
-        <li><a href="logout" class="logout"><i class="fas fa-sign-out-alt"></i> <span Multi_lang="menu_logout"></span></a></li>
+        <li><a href="Profile"><i class="fas fa-user-circle"></i> <span Multi_lang="menu_profile"></span></a></li>
+        <li><a href="Logout" class="logout"><i class="fas fa-sign-out-alt"></i> <span Multi_lang="menu_logout"></span></a></li>
     </ul>
 </aside>
 ';
@@ -76,13 +76,13 @@ switch ($_SESSION['level']) {
         <li class="menu-title" Multi_lang="main_menu"></li>
 
 <li><a href="dashboard" class="active"><i class="fas fa-home"></i> <span Multi_lang="menu_home"></span></a></li>
-        <li><a href="current_requests"><i class="fas fa-tasks"></i> <span Multi_lang="menu_current_requests"></span></a></li>
-        <li><a href="list_all_requests"><i class="fas fa-list"></i> <span Multi_lang="menu_list_all_requests"></span></a></li>
-        <li><a href="wallet"><i class="fas fa-wallet"></i> <span Multi_lang="menu_wallet"></span></a></li>
+        <li><a href="List_FollowUp_Request"><i class="fas fa-tasks"></i> <span Multi_lang="menu_current_requests"></span></a></li>
+        <li><a href="List_Request"><i class="fas fa-list"></i> <span Multi_lang="menu_list_all_requests"></span></a></li>
+        <li><a href="Wallet"><i class="fas fa-wallet"></i> <span Multi_lang="menu_wallet"></span></a></li>
         
         <li class="menu-title" Multi_lang="account_menu"></li>
-        <li><a href="profile"><i class="fas fa-user-circle"></i> <span Multi_lang="menu_profile"></span></a></li>
-        <li><a href="logout" class="logout"><i class="fas fa-sign-out-alt"></i> <span Multi_lang="menu_logout"></span></a></li>
+        <li><a href="Profile"><i class="fas fa-user-circle"></i> <span Multi_lang="menu_profile"></span></a></li>
+        <li><a href="Logout" class="logout"><i class="fas fa-sign-out-alt"></i> <span Multi_lang="menu_logout"></span></a></li>
     </ul>
 </aside>
 ';
@@ -105,7 +105,7 @@ switch ($_SESSION['level']) {
             <span class="status-dot"></span>
         </div>
         <div class="user-info">
-            <h4 class="user-name">'. $_SESSION['name'] .'</h4>
+            <h4 class="user-name"> سلام :'. $_SESSION['name'] .'</h4>
             <p class="user-role" Multi_lang="elderly_role"></p>
         </div>
     </div>
@@ -113,15 +113,15 @@ switch ($_SESSION['level']) {
     <ul class="sidebar-menu">
         <li class="menu-title" Multi_lang="main_menu"></li>
         <li><a href="dashboard" class="active"><i class="fas fa-home"></i> <span Multi_lang="menu_home"></span></a></li>
-        <li><a href="submit_request"><i class="fas fa-plus-circle"></i> <span Multi_lang="menu_submit_request"></span></a></li>
-        <li><a href="track_request"><i class="fas fa-search"></i> <span Multi_lang="menu_track_request"></span></a></li>
-        <li><a href="rate_doer"><i class="fas fa-star-half-alt"></i> <span Multi_lang="menu_rate_doer"></span></a></li>
-        <li><a href="wallet"><i class="fas fa-wallet"></i> <span Multi_lang="menu_wallet"></span></a></li>
+        <li><a href="Register_Request"><i class="fas fa-plus-circle"></i> <span Multi_lang="menu_submit_request"></span></a></li>
+        <li><a href="Order_FollowUp"><i class="fas fa-search"></i> <span Multi_lang="menu_track_request"></span></a></li>
+        <li><a href="Register_Score"><i class="fas fa-star-half-alt"></i> <span Multi_lang="menu_rate_doer"></span></a></li>
+        <li><a href="Wallet"><i class="fas fa-wallet"></i> <span Multi_lang="menu_wallet"></span></a></li>
         
         <li class="menu-title" Multi_lang="account_menu"></li>
-        <li><a href="profile"><i class="fas fa-user-circle"></i> <span Multi_lang="menu_profile"></span></a></li>
+        <li><a href="Profile"><i class="fas fa-user-circle"></i> <span Multi_lang="menu_profile"></span></a></li>
         <li><a href="need_verify"><i class="fas fa-shield-alt"></i> <span Multi_lang="menu_need_verify"></span></a></li>
-        <li><a href="logout" class="logout"><i class="fas fa-sign-out-alt"></i> <span Multi_lang="menu_logout"></span></a></li>
+        <li><a href="Logout" class="logout"><i class="fas fa-sign-out-alt"></i> <span Multi_lang="menu_logout"></span></a></li>
     </ul>
 </aside>
 ';
@@ -152,14 +152,14 @@ switch ($_SESSION['level']) {
     <ul class="sidebar-menu">
         <li class="menu-title" Multi_lang="main_menu"></li>
         <li><a href="dashboard" class="active"><i class="fas fa-home"></i> <span Multi_lang="menu_home"></span></a></li>
-        <li><a href="register_product"><i class="fas fa-plus"></i> <span Multi_lang="menu_register_product"></span></a></li>
+        <li><a href="Register_product"><i class="fas fa-plus"></i> <span Multi_lang="menu_register_product"></span></a></li>
 
-<li><a href="list_all_products"><i class="fas fa-list"></i> <span Multi_lang="menu_list_all_products"></span></a></li>
-        <li><a href="register_off_product"><i class="fas fa-ticket-alt"></i> <span Multi_lang="menu_register_off_product"></span></a></li>
+<li><a href="List_Product_Shop"><i class="fas fa-list"></i> <span Multi_lang="menu_list_all_products"></span></a></li>
+        <li><a href="Register_Offer"><i class="fas fa-ticket-alt"></i> <span Multi_lang="menu_register_off_product"></span></a></li>
         
         <li class="menu-title" Multi_lang="account_menu"></li>
-        <li><a href="profile_shop"><i class="fas fa-store"></i> <span Multi_lang="menu_profile_shop"></span></a></li>
-        <li><a href="logout" class="logout"><i class="fas fa-sign-out-alt"></i> <span Multi_lang="menu_logout"></span></a></li>
+        <li><a href="Profile"><i class="fas fa-store"></i> <span Multi_lang="menu_profile_shop"></span></a></li>
+        <li><a href="Logout" class="logout"><i class="fas fa-sign-out-alt"></i> <span Multi_lang="menu_logout"></span></a></li>
     </ul>
 </aside>
 ';
@@ -197,8 +197,8 @@ switch ($_SESSION['level']) {
         <li><a href="list_shop_products"><i class="fas fa-boxes"></i> <span Multi_lang="menu_list_shop_products"></span></a></li>
         
         <li class="menu-title" Multi_lang="account_menu"></li>
-        <li><a href="profile"><i class="fas fa-user-circle"></i> <span Multi_lang="menu_profile"></span></a></li>
-        <li><a href="logout" class="logout"><i class="fas fa-sign-out-alt"></i> <span Multi_lang="menu_logout"></span></a></li>
+        <li><a href="Profile"><i class="fas fa-user-circle"></i> <span Multi_lang="menu_profile"></span></a></li>
+        <li><a href="Logout" class="logout"><i class="fas fa-sign-out-alt"></i> <span Multi_lang="menu_logout"></span></a></li>
     </ul>
 </aside>
 ';
