6da9a60824c80c8749b4d30563f06f41edf8c03f
matin <matin.afzali.dev@gmail.com>
2026-05-28 17:29:05 -0800

    Improve Name Directory And Files

--- core/layout/Js/Js.php
+++ core/layout/Js/Js.php
@@ -2,4 +2,4 @@
 <script src="assets/vendor_components/apexcharts-bundle/irregular-data-series.js"></script>
 <script src="assets/icons/feather-icons/feather.min.js"></script>
 <script type="module" src="assets/js/language/main.js"></script>
-<script type="module" src="assets/js/main.js"></script>
+<script type="module" src="assets/js/Main.js"></script>
